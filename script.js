e96f278dd10b81e3ef6f894cc1b2315ba575e2e5
senCoins <83489390+senCoins@users.noreply.github.com>
2021-05-01 11:33:50 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+window.onload = init;
+
+function init() {
+    let btn = document.querySelector("#ok");
+
+    btn.addEventListener("click",showInfo);
+}
+function showInfo() {
+    let inptxt = document.querySelector("#userid");
+    let title = document.querySelector("#name1");
+    let srnCoin = document.querySelector("#senC");
+    let bio = document.querySelector('#bio1');
+    let axs = document.querySelector('#user1');
+    let tik = document.querySelector('#tik')
+    let info = inptxt.value;
+
+    if (info == "r0zsya") {
+        srnCoin.innerHTML = "10000"
+        title.innerHTML = inptxt.value
+        bio.innerHTML = "programmer"
+        axs.src = 'https://s2.uupload.ir/files/whatsapp_image_2021-04-30_at_13.29.49_qhqb.jpeg'
+        axs.style = 'width: 150px'
+        tik.style = 'display: block'
+    } else if (info == "lolo") {
+        srnCoin.innerHTML = "2233"
+        title.innerHTML = inptxt.value
+        bio.innerHTML = "programmer"
+        axs.src = 'https://s2.uupload.ir/files/kon_tkl4.jpg'
+        axs.style = 'width: 150px'
+        tik.style = 'display: none'
+    } else if (info == "siavashz83") {
+        srnCoin.innerHTML = "10"
+        title.innerHTML = inptxt.value
+        bio.innerHTML = "Apparatus"
+        axs.src = 'user.jpg'
+        axs.style = 'width: 150px'
+        tik.style = 'display: none'
+    } else if (info == "kaneki") {
+        srnCoin.innerHTML = "20"
+        title.innerHTML = inptxt.value
+        bio.innerHTML = "youtuber"
+        axs.src = 'user.jpg'
+        axs.style = 'width: 150px'
+        tik.style = 'display: none'
+    } else {
+        alert("چنین نام کاربری وجود ندارد جهت ساخت نام کاربری تیکت ارسال کنید!")
+    }
+}
