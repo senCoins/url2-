a00ccef6f0c7724232767ffc388ddb7a486b8988
r0zsya <83489390+senCoins@users.noreply.github.com>
2021-05-13 13:45:54 +0430

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function showInfo() {
         axs.style = 'width: 150px'
         tik.style = 'display: none'
     } else if (info == "tanyaalmasy") {
-        srnCoin.innerHTML = "420"
+        srnCoin.innerHTML = "0"
         title.innerHTML = inptxt.value
         bio.innerHTML = "none"
         axs.src = 'user.jpg'
